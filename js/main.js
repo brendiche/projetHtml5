0cd5fdde985c77ad4ea329b9e1e1792d73593c5b
unknown <usrlocal@Gfixeetu103.ad.univ-angers.fr>
2015-02-25 15:30:28 +0100

    maj dialog

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
+var yetVisited = localStorage['visited'];
+    if (!yetVisited) {
+        // open popup
+        localStorage['visited'] = "yes";
+
+
+        $( "#dialog-form" ).dialog({
+	      autoOpen: true,
+	      height: 300,
+	      width: 350,
+	      modal: true,
+	      
+	      }
+	    );
+ 
+    }
+
 
 });
